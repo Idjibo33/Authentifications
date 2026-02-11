f6df23f81240e8930b8a27e54e3b6d2ffcf649fe
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-02-11 18:16:50 +0100

    Ajout projet authentification rest API

--- firebase/lib/Services/Authentification Services/connexion_service.dart
+++ firebase/lib/Services/Authentification Services/connexion_service.dart
@@ -3,14 +3,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ConnexionService {
-  final AuthService authService = AuthService();
+  final AuthService _authService = AuthService();
   //Connecter l'utilisateur
   Future<UserCredential?> connecterUtilisateur({
     required String email,
     required String password,
   }) async {
     try {
-      return await authService.firebaseAuth.signInWithEmailAndPassword(
+      return await _authService.firebaseAuth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
